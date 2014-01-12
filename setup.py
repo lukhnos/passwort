64a41b35890cda70f50936e87352d1cb98e26b4a
Lukhnos Liu <lukhnos@lukhnos.org>
2014-01-12 13:11:05 -0800

    Convert tabs to spaces

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-	name='passwort',
-	version='1.0',
-	author = 'Lukhnos Liu',
+    name='passwort',
+    version='1.0',
+    author = 'Lukhnos Liu',
     author_email = 'lukhnos@lukhnos.org',
-	license='MIT',
-	packages=['passwort'],
-	install_requires=[
-		'pwgen >= 0.4',
-		'pycrypto >= 2.6.1'
-	],
-	test_suite='passwort.tests',
-	entry_points={
+    license='MIT',
+    packages=['passwort'],
+    install_requires=[
+        'pwgen >= 0.4',
+        'pycrypto >= 2.6.1'
+    ],
+    test_suite='passwort.tests',
+    entry_points={
         'console_scripts': ['passwort = passwort:main']
     })
