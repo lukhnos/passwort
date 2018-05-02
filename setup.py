7881389da6b17ccfe83a864ee2a455767f3c6269
Lukhnos Liu <lukhnos@lukhnos.org>
2018-05-02 16:59:31 -0700

    Supply missing project metadata

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup
 setup(
     name='passwort',
     version='1.1',
+    description='A simple password manager',
     author='Lukhnos Liu',
     author_email='lukhnos@lukhnos.org',
+    url='https://github.com/lukhnos/passwort',
     license='MIT',
     packages=['passwort'],
     install_requires=[
